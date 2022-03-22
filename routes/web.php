0a68bae98dbe5291e42285093ec3310ca8e911b9
hanie-KSM <haniesmile@gmail.com>
2022-03-22 10:07:28 +0800

    init

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Http\Controllers\TestController;
+use App\Http\Controllers\ToDoController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//Route:: get ('/trainer', function(){
+//return ('Jabatan Keselamatan Dan Kesihatan Pekerjaan');
+//});
+
+//Route::get('trainer', [TestController::class, 'cubaan']);
+
+//Route::get('/welcome', function(){
+    //return view('welcome');
+//});
+
+Route::get('todos',[ToDoController::class, 'index']);
